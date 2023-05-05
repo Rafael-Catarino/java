a42c3711fe6856b490300b597f686e76dab42a81
rafael <rafaelscatarino@hotmail.com>
2023-05-05 16:05:11 -0300

    adicinando get e set na class Jogador

--- OOP/Jogador/Jogador.java
+++ OOP/Jogador/Jogador.java
@@ -1,10 +1,32 @@
 public class Jogador {
+  private final int maxVidas = 3;
   private int num = 0;  // Sempre optar por criar atributos como private.
   private int vidas = 0;
 
-  public Jogador(int num, int vidas ) {
+  public Jogador(int num) {
     this.num = num;
-    this.vidas = vidas;
-    System.out.printf("Jogador %d criado tem %d vidas.\n",num, vidas);
+    this.vidas = 0;
+    System.out.printf("Jogador %d foi criado.\n",num);
+  }
+
+  public int getVidas() {
+    return vidas;
+  }
+
+  /* public void setVidas(int vidas) {
+    if(vidas > maxVidas) {
+      this.vidas = maxVidas;
+    } else if (vidas < 0) {
+      this.vidas = 0;
+    } else {
+      this.vidas = vidas;
+    }
+    
+  } */
+
+  public void addVidas() {
+    if(this.vidas < maxVidas){
+      this.vidas++;
+    }
   }
 }
--- OOP/Jogador/ProgramaJogador.java
+++ OOP/Jogador/ProgramaJogador.java
@@ -1,9 +1,19 @@
 public class ProgramaJogador {
   public static void main(String[] args) {
     int count = 0;
-    Jogador j1 = new Jogador(++count, 1);
-    Jogador j2 = new Jogador(++count, 2);
-    Jogador j3 = new Jogador(++count, 9);
+    Jogador j1 = new Jogador(++count);
+    Jogador j2 = new Jogador(++count);
+    Jogador j3 = new Jogador(++count);
+
+    j1.addVidas();
+    j1.addVidas();
+    j1.addVidas();
+    j1.addVidas();
+    j2.addVidas();
+    j3.addVidas();
+    System.out.printf("O numero de vidas do jagador 1 é: %d\n", j1.getVidas());
+    System.out.printf("O numero de vidas do jagador 2 é: %d\n", j2.getVidas());
+    System.out.printf("O numero de vidas do jagador 3 é: %d\n", j3.getVidas());
 
     /* j1.num = 10;
     j2.num = 5;
